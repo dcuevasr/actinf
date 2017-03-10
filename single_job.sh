553ca4351fbac3c8790d756f45f2eb551ccf1020
Dario Cuevas Rivera <dario.cuevas_rivera@tu-dresden.de>
2017-03-10 12:13:07 +0100

    Added the new bash file to run many subjects

--- single_job.sh
+++ single_job.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#SBATCH --array=1-5
-#SBATCH --time=10   # If only one number, it is assumed to be minutes
+#SBATCH --array=1-840
+#SBATCH --time=30   # If only one number, it is assumed to be minutes
 #SBATCH --ntasks=1
 #SBATCH --partition=sandy,west
 #SBATCH --mem-per-cpu=2G
 #SBATCH -J "p_pdm_kolling"
 #SBATCH --mail-user=dario.cuevas_rivera@tu-dresden.de
-#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-type=END
 #SBATCH -A p_pdm
-#SBATCH --output=slurm-%A_%a.out
+#SBATCH --output=./data/logs/slurm-%A_%a.out
 
-SUBJNUMBER=0
+SUBJNUMBER=1
 module load python/3.5.2-anaconda4.2.0
-python /projects/p_pdm/invert_parameters -v -i $((SLURM_ARRAY_TASK_ID-1)) $SUBJNUMBER
+python /home/cuevasri/kolling/actinf/invert_parameters.py -v -i $((SLURM_ARRAY_TASK_ID-1)) $SUBJNUMBER
